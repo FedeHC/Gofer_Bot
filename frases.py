fe96480dd88fa8b7121801789e660e9162888c32
FedeHC <fede.hc@mail.com>
2018-10-01 15:53:59 -0300

    Cambiando frases

--- frases.py
+++ frases.py
@@ -6,46 +6,43 @@
 # -------------------------------
 
 mensaje_ayuda = """⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️
-⬜️⬜️⬜️⬜️⬜️ [ A Y U D A ] ⬜️⬜️⬜️⬜️⬜️⬜️⬜️
+⬜️⬜️⬜️⬜️⬜️⬜️ [ A Y U D A ] ⬜️⬜️⬜️⬜️⬜️⬜️
 ⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️
-⬜️ /ayuda —> Esta mierda que estas viendo.⬜️⬜️
-⬜️ /arranca —> Arranca el bot.                        ⬜️⬜️
-⬜️ /dilotuyo —> La frase random.                   ⬜️⬜️
+⬜️ /ayuda —> Esta mierda que estas viendo.      ⬜️
+⬜️ /clima —> Tira datos del clima.                         ⬜️
+⬜️ /dilotuyo —> La frase random que querias.   ⬜️
 ⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️
 ⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️"""
 
 
 frases = [
 "Basta Fede.",
-"No más commits por hoy.",
-"✨ Ｐ ｅ ｎ ｅ ｔ ｒ ａ ｔ ｉ ｏ ｎ   Ｔ ｅ ｓ ｔ ｉ ｎ ｇ  ✨",
+"No más commits por hoy su señoría.",
 "Fran, estamos abajo...",
 "Abrí la puerta Fraaaaaaaannnnnnnnnnnnnnnnnnn!",
-"LPM! Otra vez me fui para La Maquinita, arrggg! :(",
+"LPM, otra vez me fui para La Maquinita... arrggg. :(",
 "Mergeamela toda.",
 "No sean giles y comenten bien sus commits.",
-"¿No tenes nada mejor que hacer que andar tirando /dilotuyo's todo el tiempo? 😑",
-"No quiero decir nada peroooo... uds. hablan muy fuerte en el coworking.  👮‍♀️",
+"¿No tenes nada mejor que hacer?",
+"No quiero decir nada pero están hablando muy fuerte en el coworking...  👮‍♀️",
 "¿Sabías que el nuevo coworking se llama Hashtag Coworking?\n¿Y que antiguamente era una unidad básica peronista?",
-"No se me ocurre nada ahora.",
-"Seria una lástima que a tu notebook se le darramara todo el café... ಠ⌣ಠ",
+"No se me ocurre nada.",
+"Seria una lástima que a tu compu se le cayera tooodo el café... ಠ⌣ಠ ☕",
 "Extraño la sala 5. :')",
-"👀",
+"⬜️⬜️⬜️⬜️⬜️⬜️\n⬜️ Vale por       ⬜️\n⬜️ 1 vida. ❤️    ⬜️\n⬜️⬜️⬜️⬜️⬜️⬜️",
 "¿Sabías queCHUPALA GLOVO, CHUPALA RAPPI, CHUPENLA TODOS AARRRRRRGGG",
 "Glovo se la come.",
 "Rappi se la come.",
 "Pedidos Ya se la come.",
-"🔥🔥\n😭💻",
 "Vayan a codear.",
-"🔥🔥🔥\n🔥🙂🔥 —> This is fine.\n🔥🔥🔥",
 "Puto el que lee.",
-"⬜️⬜️⬜️⬜️⬜️\n⬜️ Vale por ⬜️\n⬜️ 1 trabajo⬜️\n⬜️⬜️⬜️⬜️⬜️",
+"⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n⬜️   Vale por           ⬜️\n⬜️   1 sueldo. 💵   ⬜️\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️",
 "Si estas viendo este /dilotuyo\nmientras scrolleabas este chat\n\n✨✨✨🤚🏻🤖✋🏻✨✨✨\nFuiste visitado por el ANGEL\nROBOTICO DE LOS TRAINEES\n\nSuerte asegurada en tu prox.\nentrevista solo si posteas\n✨ 'Gracias GoferBot'✨✨",
 "Dejame descansar un poco che.",
 "Qué ganas de romper las bolas ehh.",
 "El cielo oscurecee 🎵\nY el 28 no vieneee... 🎶",
 "Juraría que habia arreglado ese bug.",
-"Hagan algo productivo con sus vidas.",
+"Hacé algo productivo con tu vida, no rompas.",
 "Ｃ Ｈ Ｕ Ｐ Ａ Ｌ Ａ",
 "https://twitter.com/GoferAr",
 "¿¿No conoces la dignidad cuando la ves??  \\(ಠ╭╮ಠ)",
@@ -58,12 +55,15 @@ frases = [
 "Ｓ Ａ Ｒ Ｌ Ａ Ｎ Ｇ Ａ",
 "¿Hay clases el miércoles?",
 "https://github.com/FedeHC/Gofer_Bot",
-"Qué dura es la vida del trainee... (?)",
-"Ese TP no se va a hacer solo.",
-"Yo sé que me querés. 😘",
+"Qué dura es la vida del trainee.",
+"Ese TP no se va a terminar de hacer solo...",
+"Dale, yo sé que me querés. 😘",
 "Ponele ganas a esa materia, dale.",
-"Te iba a preguntar algo pero se me olvidó.",
-"Ponele que... nah, no dije nada.",
+"Te iba a preguntar algo pero se olvidé.",
+"Ponele que si.",
 "¿Sale café de máquina? ✨☕️✨",
-"A Globant tengo ganas de cascotearle el edificio de la esquina."
+"A Globant tengo ganas de cascotearle el edificio de la esquina.",
+"⬜️⬜️⬜️⬜️⬜️⬜️\n⬜️  Vale por      ⬜️\n⬜️  1 café. ☕️   ⬜️\n⬜️⬜️⬜️⬜️⬜️⬜️",
+"Espero que no llueva.",
+"Llevá la notebook."
 ]
